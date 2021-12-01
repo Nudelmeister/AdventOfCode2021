37e6ee3d66ba1b633aa07389d96588c9c065c771
Nudelmeister <leon@stichternath.net>
2021-12-02 00:27:53 +0100

    fix bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,7 +9,7 @@ criterion_group!(
 );
 
 fn benchmark_01_sonarsweep(c: &mut Criterion) {
-    let input = day_01::read_input("inputs/01-sonarsweep.txt").collect::<Vec<_>>();
+    let input = day_01::read_input("inputs/01.txt").collect::<Vec<_>>();
     let mut group = c.benchmark_group("Puzzle 01 - Sonar Sweep");
     group.bench_function("Part 1", |b| {
         b.iter_batched(
